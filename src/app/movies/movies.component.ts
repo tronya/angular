413bb17f603095ed29e58997d2096dded42e6cf7
y.troniak <y.troniak@gmail.com>
2020-04-01 16:58:02 +0300

    Movies

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,18 +1,27 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Store} from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
 import * as moviesActions from './store/movies.actions';
 import {Subscription} from 'rxjs';
 import {IMovie, IMovies, MovieItem} from './movies.model';
 
+enum Direction {
+  Down,
+  Up = 1,
+}
+
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss']
 })
+
 export class MoviesComponent implements OnInit, OnDestroy {
+  @ViewChild('moviesContainer', {static: false}) moviesContainer: any;
   movies: IMovie[] = [];
   loading = false;
+  scrollTop = 0;
+  scrollingActive = true;
 
   private moviesSub: Subscription;
 
@@ -31,6 +40,32 @@ export class MoviesComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.moviesSub.unsubscribe();
+  }
+
+  switchSlide(direction: Direction) {
+    console.log(direction);
+    this.scrollingActive = false;
+  }
+
+  checkDirection(scrollValue) {
+    if (scrollValue === this.scrollTop) {
+      return false;
+    }
+    if (this.scrollingActive) {
+      if (scrollValue > this.scrollTop) {
+        this.switchSlide(Direction.Up);
+      } else {
+        this.switchSlide(Direction.Down);
+      }
+    }
+  }
+
+  scrollById(id: number) {
+    document.getElementById(`movie_id_${id}`).scrollIntoView({block: 'center', behavior: 'smooth'});
+  }
+
+  refresh(event) {
+    // this.checkDirection(event.target.scrollTop);
   }
 
 }
